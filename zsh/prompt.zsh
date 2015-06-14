f55817e2c43a586a950f63a43567847d82b478f6
Alexey Shein <alexey.shein.dev@gmail.com>
2015-06-14 15:41:37 +0500

    Make prompt oneline
    Fix extra whitespaces when not in git repository.

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -20,9 +20,9 @@ git_dirty() {
   else
     if [[ $($git status --porcelain) == "" ]]
     then
-      echo "on %{$fg_bold[green]%}$(git_prompt_info)%{$reset_color%}"
+      echo " on %{$fg_bold[green]%}$(git_prompt_info)%{$reset_color%}"
     else
-      echo "on %{$fg_bold[red]%}$(git_prompt_info)%{$reset_color%}"
+      echo " on %{$fg_bold[red]%}$(git_prompt_info)%{$reset_color%}"
     fi
   fi
 }
@@ -70,8 +70,7 @@ rb_prompt() {
 directory_name() {
   echo "%{$fg_bold[cyan]%}%~%{$reset_color%}"
 }
-
-export PROMPT=$'\n$(rb_prompt)in $(directory_name) $(git_dirty)$(need_push)\n› '
+export PROMPT=$'$(rb_prompt)in $(directory_name)$(git_dirty)$(need_push)› '
 set_prompt () {
   export RPROMPT="%{$fg_bold[cyan]%}%{$reset_color%}"
 }
