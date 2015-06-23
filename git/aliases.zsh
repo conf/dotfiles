a82a34b78062f0fd372f95f4dcc4fb773ea17c55
!printenv GIT_AUTHOR_NAME <!printenv GIT_AUTHOR_EMAIL>
2015-06-23 18:24:33 +0500

    Add my git aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -7,13 +7,14 @@ then
 fi
 
 # The rest of my fun git aliases
-alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin HEAD'
 alias gd='git diff'
-alias gc='git commit'
-alias gca='git commit -a'
-alias gco='git checkout'
-alias gcb='git copy-branch-name'
+alias go='git checkout'
 alias gb='git branch'
-alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
+alias gs='git status -sb'
+alias ga='git add'
+alias gau='ga -u'
+alias gaa='ga -A'
+alias gk='gitk --all &'
+alias gcb='git copy-branch-name'
