9e29c1157aaec133dda25e5cad812fc647aa7186
Alexey Shein <alexey.shein.dev@gmail.com>
2016-02-02 16:09:15 +0500

    Add psgrep

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -22,6 +22,7 @@ fi
 # Install homebrew packages
 brew install grc coreutils the_silver_searcher homebrew/dupes/grep git imagemagick wget
 brew install openssl node
+brew install psgrep
 
 # we need java for elasticsearch
 brew install Caskroom/cask/java
