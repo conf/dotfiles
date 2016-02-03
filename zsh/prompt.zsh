e356897e10f4db4e37fa12e64ed919a4027fc3be
Alexey Shein <alexey.shein.dev@gmail.com>
2016-02-04 01:26:32 +0500

    Refactor git prompt functions

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -10,25 +10,27 @@ else
 fi
 
 git_branch() {
-  echo $($git symbolic-ref HEAD 2>/dev/null | awk -F/ {'print $NF'})
+  $git symbolic-ref --short HEAD 2>/dev/null
+}
+
+has_tracking_branch() {
+ $git rev-parse --abbrev-ref @{u} > /dev/null 2>&1
+}
+
+inside_git() {
+  $git rev-parse --is-inside-work-tree > /dev/null 2>&1
 }
 
 git_dirty() {
-  if $($git rev-parse --is-inside-work-tree 2> /dev/null)
+  inside_git || return
+  if [[ $($git status --porcelain) == "" ]]
   then
-    if [[ $($git status --porcelain) == "" ]]
-    then
-      echo " on %{$fg_bold[green]%}$(git_prompt_info)%{$reset_color%}"
-    else
-      echo " on %{$fg_bold[red]%}$(git_prompt_info)%{$reset_color%}"
-    fi
+    branch_color="green"
+  else
+    branch_color="red"
   fi
-}
 
-git_prompt_info () {
- ref=$($git symbolic-ref HEAD 2>/dev/null) || return
-# echo "(%{\e[0;33m%}${ref#refs/heads/}%{\e[0m%})"
- echo "${ref#refs/heads/}"
+  echo " on %{$fg_bold[$branch_color]%}$(git_branch)%{$reset_color%}"
 }
 
 unpushed () {
