edc4bd22d3099e0cf98682bbf10b1fddd7c754f7
Alexey Shein <alexey.shein.dev@gmail.com>
2017-04-01 16:58:43 +0300

    Make sure aliased commands use autocompletion

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -38,6 +38,7 @@ setopt APPEND_HISTORY # adds history
 setopt INC_APPEND_HISTORY SHARE_HISTORY  # adds history incrementally and share it across sessions
 setopt HIST_IGNORE_ALL_DUPS  # don't record dupes in history
 setopt HIST_REDUCE_BLANKS
+unsetopt COMPLETE_ALIASES
 
 zle -N newtab
 
