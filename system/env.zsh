58d4ea7225eabd23d7e57a4531ac94129260fc28
Alexey Shein <alexey.shein.dev@gmail.com>
2020-02-26 01:14:15 +0500

    Make less read archives as well

--- system/env.zsh
+++ system/env.zsh
@@ -2,6 +2,8 @@ export EDITOR='nvim'
 
 # let's teach less some colors
 export LESS='-FSRX'
+# and uncompress files
+export LESSOPEN="|/usr/local/bin/lesspipe.sh %s" LESS_ADVANCED_PREPROCESSOR=1
 
 export LC_ALL="en_US.UTF-8"
 export LANG="en_US.UTF-8"
