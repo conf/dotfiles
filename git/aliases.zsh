3f6d6dd8a351a9514801d16a43ae8abece333234
Alexey Shein <alexey.shein@toptal.com>
2022-01-17 13:15:35 +0500

    Use goc alias to avoid conflict with go executable

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -12,7 +12,7 @@ alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C
 alias gp='git push $(has_tracking_branch || echo "-u origin $(git_branch)")'
 alias gpr='$(has_tracking_branch) || git push -u origin $(git_branch) && hub pull-request -o -l wip -p -a conf'
 alias gd='git diff'
-alias go='git checkout'
+alias goc='git checkout'
 alias gb='git branch'
 alias gs='git status -sb'
 alias ga='git add'
