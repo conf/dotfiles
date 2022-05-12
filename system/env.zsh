bb57350f636ea28494e9b8b8e2691dbf4ee719e4
Alexey Shein <aleksey.shein@vestiairecollective.com>
2022-05-12 12:53:37 +0200

    Make homebrew path generic

--- system/env.zsh
+++ system/env.zsh
@@ -3,7 +3,7 @@ export EDITOR='nvim'
 # let's teach less some colors
 export LESS='-FSRX'
 # and uncompress files
-export LESSOPEN="|/usr/local/bin/lesspipe.sh %s" LESS_ADVANCED_PREPROCESSOR=1
+export LESSOPEN="|$(which lesspipe.sh) %s" LESS_ADVANCED_PREPROCESSOR=1
 
 export LC_ALL="en_US.UTF-8"
 export LANG="en_US.UTF-8"
