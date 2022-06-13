071c736627bf739f69355209d369a86954a06b70
Alexey Shein <alexey.shein.dev@gmail.com>
2022-06-13 15:08:07 +0200

    Make fzf search with ag

--- vim/env.zsh
+++ vim/env.zsh
@@ -1,7 +1,4 @@
-# --files: List files that would be searched but do not search
-# --no-ignore: Do not respect .gitignore, etc...
-# --hidden: Search hidden files and folders
-# --follow: Follow symlinks
-# --glob: Additional conditions for search (in this case ignore everything in the .git/ folder)
-export FZF_DEFAULT_COMMAND='rg --files --hidden --follow '
+export FZF_DEFAULT_COMMAND='ag -l --path-to-ignore ~/.ignore --nocolor --hidden -g ""'
+# To install useful keybindings and fuzzy completion:
+#  $(brew --prefix)/opt/fzf/install
 
