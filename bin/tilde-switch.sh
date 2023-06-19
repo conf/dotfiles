ebdb55ad49ab3377d6c04a29f9b23aa274b11b67
Alexey Shein <alexey.shein.dev@gmail.com>
2023-06-19 16:04:00 +0200

    Fix starship status

--- bin/tilde-switch.sh
+++ bin/tilde-switch.sh
@@ -2,7 +2,16 @@
 # Swap § and ~ keys 
 # credits: https://ppolyzos.com/2020/10/09/swap-places-between-tilde-and-section-sign-%C2%A7-key-in-your-macbook-keyboard/
 
-hidutil property --set '{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x700000035,"HIDKeyboardModifierMappingDst":0x700000064},{"HIDKeyboardModifierMappingSrc":0x700000064,"HIDKeyboardModifierMappingDst":0x700000035}]}'
+set -euo pipefail
+
+# check if the command returns "(<eol>)"
+if [[ "$(hidutil property --get UserKeyMapping)" == $'(\n)' ]]; then
+
+  # no existing mapping found, we need to remap the keys
+  hidutil property --set '{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x700000035,"HIDKeyboardModifierMappingDst":0x700000064},{"HIDKeyboardModifierMappingSrc":0x700000064,"HIDKeyboardModifierMappingDst":0x700000035}]}' > /dev/null
+fi
 
 # how to undo:
+# hidutil property --set '{"UserKeyMapping":[]}'
+# or
 # hidutil property --set '{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x700000035,"HIDKeyboardModifierMappingDst":0x700000035},{"HIDKeyboardModifierMappingSrc":0x700000064,"HIDKeyboardModifierMappingDst":0x700000064}]}'
