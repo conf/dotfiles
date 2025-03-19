3f208665b42d17223e90fa105c5e1ad895853d61
Aleksey Shein <alexey.shein.dev@gmail.com>
2025-03-19 14:52:26 +0100

    Chop long-lines by default in less

--- system/env.zsh
+++ system/env.zsh
@@ -1,7 +1,7 @@
 export EDITOR='nvim'
 
 # let's teach less some colors
-export LESS='-FRXi'
+export LESS='-FRXSi'
 # and uncompress files
 export LESSOPEN="|$(which lesspipe.sh) %s" LESS_ADVANCED_PREPROCESSOR=1
 
