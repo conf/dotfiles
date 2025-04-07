c16c9f09bbfac3a5531c16e507a0c5d6d3ea4ad9
Aleksey Shein <alexey.shein.dev@gmail.com>
2025-04-07 13:24:19 +0200

    Fix tilde-switch check again

--- bin/tilde-switch.sh
+++ bin/tilde-switch.sh
@@ -4,13 +4,14 @@
 
 set -euo pipefail
 
-# check if the command returns "(<eol>)"
-if [[ "$(hidutil property --get UserKeyMapping)" == "$(printf "(\n)")" ]]; then
-  echo 'Remapping § to `'
+USER_KEY_MAPPING="$(hidutil property --get UserKeyMapping)"
 
+# check if the command returns "(<eol>)"
+if [[ "$USER_KEY_MAPPING" == $'(null)' || "$USER_KEY_MAPPING" == "$(printf "(\n)")" ]]; then
+  echo 'Remapping § to `'
   # no existing mapping found, we need to remap the keys
   hidutil property --set '{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x700000035,"HIDKeyboardModifierMappingDst":0x700000064},{"HIDKeyboardModifierMappingSrc":0x700000064,"HIDKeyboardModifierMappingDst":0x700000035}]}' > /dev/null
-  echo "Done!"
+  echo 'Done!'
 fi
 
 # how to undo:
