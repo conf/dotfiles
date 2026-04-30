c9170373e352c783db6869d73ed6851ad85d2f25
Aleksey Shein <alexey.shein.dev@gmail.com>
2026-04-30 23:23:50 +0200

    disable starship

--- starship/env.zsh
+++ starship/env.zsh
@@ -1,2 +1,2 @@
-export STARSHIP_CONFIG="$HOME/.dotfiles/starship/starship.toml"
-eval "$(starship init zsh)"
+#export STARSHIP_CONFIG="$HOME/.dotfiles/starship/starship.toml"
+#eval "$(starship init zsh)"
