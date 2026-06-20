4e0ec833fd1096346bce444b3c651528cbee5610
Aleksey Shein <alexey.shein.dev@gmail.com>
2026-06-20 09:18:28 +0200

    Tilde switch is not needed anymore

--- tilde-switch/remap.zsh
+++ tilde-switch/remap.zsh
@@ -1,2 +1,2 @@
 # Remap § ↔ ` (mapping resets on reboot; script itself skips if already applied)
-"$HOME/.dotfiles/bin/tilde-switch.sh" >/dev/null
+# "$HOME/.dotfiles/bin/tilde-switch.sh" >/dev/null
